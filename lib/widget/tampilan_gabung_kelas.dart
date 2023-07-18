972c887ffdc2d433c4d0e7d51787ce116670d35a
RofiqAbdulRozak <rofiqrozak@gmail.com>
2023-07-18 16:35:33 +0700

    tampilan_gabung_kelas Fixes #10 sudah selesai(adi pangestu)

---
+++ lib/widget/tampilan_gabung_kelas.dart
@@ -0,0 +1,217 @@
+import 'package:flutter/material.dart';
+
+class tampil_gabung_kelas extends StatefulWidget {
+  const tampil_gabung_kelas({Key? key}) : super(key: key);
+
+  @override
+  _tampil_gabung_kelasState createState() => _tampil_gabung_kelasState();
+}
+
+class _tampil_gabung_kelasState extends State<tampil_gabung_kelas> {
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Bahasa Inggris Lanjut"),
+      ),
+      body: SingleChildScrollView(
+        child: Stack(
+          children: <Widget>[
+            Container(
+              height: 200,
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(25),
+                  bottomRight: Radius.circular(25),
+                ),
+              ),
+            ),
+            Container(
+              height: 100,
+              width: screenWidth,
+              margin: EdgeInsets.only(
+                top: 50,
+                left: 30,
+              ),
+              child: Text(
+                "Hai Rofiq",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Container(
+              height: 50,
+              width: 50,
+              margin: EdgeInsets.only(
+                top: 30,
+                left: screenWidth - 80,
+              ),
+              child: IconButton(
+                onPressed: () {
+                  // Navigator.push(
+                  //   context,
+                  //   MaterialPageRoute(builder: (context) => my_profil()),
+                  // );
+                },
+                icon: Icon(
+                  Icons.person,
+                  color: Colors.white,
+                ),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.yellow,
+                borderRadius: BorderRadius.circular(25),
+              ),
+            ),
+            Stack(
+              children: [
+                Container(
+                  height: 150,
+                  width: screenWidth - 60,
+                  margin: EdgeInsets.only(
+                    top: 100,
+                    left: 30,
+                    right: 30,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Bahasa Inggris Lanjut\nIF21A\nDini Riandini",
+                        style: TextStyle(
+                          fontSize: 17,
+                          color: Colors.blue,
+                        ),
+                      ),
+                      Image.asset(
+                        'assets/logo2.png',
+                        height: 80,
+                        width: 80,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  height: 30,
+                  width: screenWidth / 3 - 30,
+                  margin: EdgeInsets.only(
+                    top: 270,
+                    right: 10,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: TextButton(
+                    onPressed: () {
+                      // Navigator.push(
+                      //   context,
+                      //   MaterialPageRoute(
+                      //       builder: (context) => tampil_buat_kelas()),
+                      // );
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(width: 5),
+                        Text(
+                          "Informasi",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 30,
+                  width: screenWidth / 3 - 30,
+                  margin: EdgeInsets.only(
+                    top: 270,
+                    right: 10,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: TextButton(
+                    onPressed: () {
+                      // Navigator.push(
+                      //   context,
+                      //   MaterialPageRoute(
+                      //       builder: (context) => lihat_tugas_dosen()),
+                      // );
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(width: 5),
+                        Text(
+                          "Tugas",
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 30,
+                  width: screenWidth / 3 - 30,
+                  margin: EdgeInsets.only(
+                    top: 270,
+                    right: 10,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: TextButton(
+                    onPressed: () {
+                      // Navigator.push(
+                      //   context,
+                      //   MaterialPageRoute(
+                      //       builder: (context) => anggota_dosen()),
+                      // );
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(width: 5),
+                        Text(
+                          "Anggota",
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

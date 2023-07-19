f5030906c163f3c2bdda2013d3c75dd01ae43d1b
Meisy-Nur-Laely <meisynurlaely@gmail.com>
2023-07-19 08:00:45 +0700

    menu_daftar_kelas Fixes #6

---
+++ lib/kelas/menu_daftar_kelas.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'buat_kelas.dart';
+// import 'tampil_notes.dart';
+
+class menu_daftar_kelas extends StatefulWidget {
+  const menu_daftar_kelas({Key? key});
+
+  @override
+  State<menu_daftar_kelas> createState() => _menu_daftar_kelasState();
+}
+
+class _menu_daftar_kelasState extends State<menu_daftar_kelas> {
+  // void navigateToTampilNotes(String noteId) {
+  //   Navigator.push(
+  //     context,
+  //     MaterialPageRoute(builder: (context) => tampil_notes(noteId: noteId)),
+  //   );
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Daftar kelas"),
+      ),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(
+              top: 30,
+              left: 30,
+            ),
+            child: Text(
+              "Daftar kelas",
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.blue,
+              ),
+            ),
+          ),
+          StreamBuilder<QuerySnapshot>(
+            stream: FirebaseFirestore.instance.collection('kelas').snapshots(),
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
+              }
+
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return CircularProgressIndicator();
+              }
+
+              final kelasDocs = snapshot.data!.docs;
+
+              return ListView.builder(
+                padding: EdgeInsets.only(
+                  top: 120,
+                  left: 30,
+                  right: 30,
+                ),
+                itemCount: kelasDocs.length,
+                itemBuilder: (context, index) {
+                  final kelasData =
+                      kelasDocs[index].data() as Map<String, dynamic>;
+                  final kelas = kelasData['kelas'];
+                  final matakuliah = kelasData['matakuliah'];
+
+                  return InkWell(
+                    // onTap: () => navigateToTampilNotes(noteId),
+                    child: Container(
+                      height: 100,
+                      margin: EdgeInsets.only(bottom: 16),
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Icon(
+                              Icons.class_,
+                              color: Colors.white,
+                              size: 80,
+                            ),
+                          ),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(bottom: 8.0),
+                                child: Text(
+                                  '$kelas',
+                                  style: TextStyle(
+                                    fontSize: 19,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                              Text(
+                                '$matakuliah',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.white,
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
